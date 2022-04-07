283fc7a3301f48c528c5398f2ef947ad42a8fa69
EC2 Default User <ec2-user@ip-172-31-31-175.eu-west-2.compute.internal>
2022-04-07 17:36:57 +0000

    ran terraform fmt

--- modules/eventbridge/eventbridge.tf
+++ modules/eventbridge/eventbridge.tf
@@ -1,7 +1,7 @@
 
 resource "aws_cloudwatch_event_rule" "lambda_event_rule" {
-  name = "lambda-event-rule"
-  description = "run at every account creation"
+  name          = "lambda-event-rule"
+  description   = "run at every account creation"
   event_pattern = <<EOF
   {
     "eventName": [
@@ -21,14 +21,14 @@ resource "aws_cloudwatch_event_rule" "lambda_event_rule" {
 }
 
 resource "aws_cloudwatch_event_target" "lambda_target" {
-  arn = var.LambdaARN
+  arn  = var.LambdaARN
   rule = aws_cloudwatch_event_rule.lambda_event_rule.name
 }
-  
+
 resource "aws_lambda_permission" "allow_cloudwatch_to_trigger_lambda" {
-  statement_id = "AllowExecutionFromCloudWatch"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
   function_name = var.LambdaName
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.lambda_event_rule.arn
-} 
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_event_rule.arn
+}
